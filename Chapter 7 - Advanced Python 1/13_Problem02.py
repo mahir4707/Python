76ba1129a607c32a33c928063c1e822fb327af13
mahir4707 <mahirsama68@gmail.com>
2025-12-13 10:46:18 +0530

    added chapter 7

--- Chapter 7 - Advanced Python 1/13_Problem02.py
+++ Chapter 7 - Advanced Python 1/13_Problem02.py
@@ -1,7 +1,7 @@
-# program to print third, fifth and seventh element of a list using enumerate function
+# program to print third, fifth and eight element of a list using enumerate function
 
 my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
 for index, value in enumerate(my_list):
-    if index in (2, 4, 6):  
+    if index in (2, 4, 7):  
         print(value) 
